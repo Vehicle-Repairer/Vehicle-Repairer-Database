9d0621c2755c4ffee54613d04a1280887db69baf
Shu-Huai <lvzhihe_123@qq.com>
2022-03-25 17:32:20 +0800

    创建索引

--- create.sql
+++ create.sql
@@ -2,48 +2,54 @@ create type sex as enum ('男', '女');
 alter type sex owner to root;
 create table if not exists customer
 (
-    cid            integer not null,
+    cid            integer not null
+        primary key,
     cname          varchar(20),
     rtype          varchar(30),
     rate           integer,
     contact_person varchar(10),
-    phone          varchar(20),
-    primary key (cid)
+    phone          varchar(20)
 );
 alter table customer
     owner to root;
 create table if not exists parts
 (
-    pid     integer not null,
+    pid     integer not null
+        primary key,
     pname   varchar(20),
     pprice  integer,
-    pnumber integer,
-    primary key (pid)
+    pnumber integer
 );
 alter table parts
     owner to root;
-create table repairman
+create table if not exists repairman
 (
-    id              integer   default nextval('repairman_id_seq'::regclass) not null,
+    id              serial
+        constraint repairman_pk
+            primary key,
     man_name        varchar(20),
     profession      varchar(10),
     hour_cost       numeric(10, 2),
-    hashed_password varchar(255)                                            not null,
+    hashed_password varchar(255) not null,
     sex             varchar(1),
     phone           varchar(11),
     birthday        timestamp(0),
     address         varchar(255),
     email_address   varchar(255),
-    account         varchar(255)                                            not null,
+    account         varchar(255) not null,
     created_time    timestamp default timezone('PRC'::text, now())
 );
 alter table repairman
     owner to root;
-create unique index repairman_account_uindex
+create unique index if not exists repairman_id_uindex
+    on repairman (id);
+create unique index if not exists repairman_account_uindex
     on repairman (account collate cstore);
-create table salesman
+create table if not exists salesman
 (
-    id              serial,
+    id              serial
+        constraint salesman_pk
+            primary key,
     man_name        varchar(20),
     hashed_password varchar(255)                                   not null,
     sex             varchar(1),
@@ -56,20 +62,21 @@ create table salesman
 );
 alter table salesman
     owner to root;
-create unique index salesman_account_uindex
+create unique index if not exists salesman_id_uindex
+    on salesman (id);
+create unique index if not exists salesman_account_uindex
     on salesman (account collate cstore);
 create table if not exists vehicle
 (
-    frame_number         varchar(17) not null,
+    frame_number         varchar(17) not null
+        primary key,
     license_plate_number varchar(7),
-    cid                  integer,
+    cid                  integer
+        references customer
+            on update cascade on delete cascade,
     color                varchar(5),
     vtype                varchar(20),
-    vehicle_type         varchar(20),
-    primary key (frame_number),
-    foreign key (cid)
-        references customer
-        on update cascade on delete cascade
+    vehicle_type         varchar(20)
 );
 alter table vehicle
     owner to root;
